20b586911de48caae4ed7bc04a15b8b9ee3fde9e
Shirshajit Sen Gupta <shirshajit@gmail.com>
2022-07-01 05:53:23 +0800

    settings page refactor

--- lib/exports.dart
+++ lib/exports.dart
@@ -2,6 +2,6 @@ export 'package:flutter_bloc/flutter_bloc.dart';
 export 'package:diffusion_gui/bloc/phase_bloc.dart';
 export 'package:diffusion_gui/screens/home.dart';
 export 'package:diffusion_gui/screens/instructions.dart';
-export 'package:diffusion_gui/settings.dart';
+export 'package:diffusion_gui/screens/settings.dart';
 export 'package:diffusion_gui/shared/routes.dart';
 export 'package:flutter/material.dart';
